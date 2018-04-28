3d8efacf0816b102435d0ce84ec22f50022f4421
Emmett Neyman <eneyman@seas.upenn.edu>
2018-04-27 20:08:25 -0400

    still doesnt work

--- insomnia.js
+++ insomnia.js
@@ -14,9 +14,9 @@ var productData = { deliveryMethod: "both",
                     productID: 4,
                     platform: "website"};
 
-var submittedData = "shipping_name=Emmett+Neyman&shipping_phone=2159003788&address=101++South+39th+Street%2C+Philadelphia%2C+PA+19104&address_2=D301&instructions=Call+when+here.&comment=&customer_name=Emmett+Neyman&payment_phone=2159003788&payment_email=emmettneyman%40gmail.com&payment_guest=Nope&location_type=off-campus&credit_card=&cash=&schoolcash=&giftcard=&tip=";
+var submittedData = 'shipping_name=Emmett+Neyman&shipping_phone=2159003788&address=101++South+39th+Street%2C+Philadelphia%2C+PA+19104&address_2=D301&instructions=Call+when+here.&comment=&customer_name=Emmett+Neyman&payment_phone=2159003788&payment_email=emmettneyman%40gmail.com&payment_guest=Nope&location_type=off-campus&credit_card=&cash=&schoolcash=&giftcard=&tip=';
 
-var orderData = "shipping_name=Emmett+Neyman&shipping_phone=2159003788&address=101++South+39th+Street%2C+Philadelphia%2C+PA+19104&address_2=D301&instructions=Call+when+here.&comment=&customer_name=Emmett+Neyman&payment_phone=2159003788&payment_email=emmettneyman%40gmail.com&payment_guest=Nope&location_type=off-campus&credit_card=&cash=&schoolcash=&giftcard=&tip=&payment_email=emmettneyman%40gmail.com&payment_phone=2159003788&order_type=2&address_1=101%20%20South%2039th%20Street&address_2=D301&city=Philadelphia&state=PA&zip=19104&remember_cards=false&width=1280";
+var orderData = 'shipping_name=Emmett+Neyman&shipping_phone=2159003788&address=101++South+39th+Street%2C+Philadelphia%2C+PA+19104&address_2=D301&instructions=Call+when+here.&comment=&customer_name=Emmett+Neyman&payment_phone=2159003788&payment_email=emmettneyman%40gmail.com&payment_guest=Nope&location_type=off-campus&credit_card=&cash=&schoolcash=&giftcard=&tip=&payment_email=emmettneyman%40gmail.com&payment_phone=2159003788&order_type=2&address_1=101%20%20South%2039th%20Street&address_2=D301&city=Philadelphia&state=PA&zip=19104&remember_cards=false&width=1280';
 
 var storeRequest = { 
     method: 'POST',
@@ -98,7 +98,7 @@ var updateTipRequest = {
 var checkSubmittedDataRequest = { 
     method: 'POST',
     url: 'https://insomniacookies.com/shop/checkSubmitedData',
-    body: submittedData,
+    body: { data: submittedData },
     json: true 
 };
 
@@ -108,7 +108,7 @@ var getStoreStatusRequest = {
     json: true
 };
 
-var canCheckoutRequest = {
+var canCheckoutRequest2 = {
     method: 'GET',
     url: 'https://insomniacookies.com/shop/canCheckout?order_type=2?payment=1',
     json: true
@@ -124,7 +124,7 @@ var addPaymentRequest = {
 var completeOrderRequest = { 
     method: 'POST',
     url: 'https://insomniacookies.com/shop/completeOrder',
-    body: orderData,
+    body: { data: orderData },
     json: true 
 };
 
@@ -142,7 +142,7 @@ var getCart = function (cb) {
     request(getCartRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('getCart');
             cb();
         }
     });
@@ -152,17 +152,17 @@ var addPayment = function () {
     request(addPaymentRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('addPayment');
             completeOrder();
         }
     });
 };
 
-var canCheckout = function () {
-    request(canCheckoutRequest, function(error, response, body) {
+var canCheckout2 = function () {
+    request(canCheckoutRequest2, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('canCheckout');
             addPayment();
         }
     });
@@ -172,8 +172,8 @@ var getStoreStatus = function () {
     request(getStoreStatusRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
-            canCheckout();
+            console.log('getStoreStatus');
+            canCheckout2();
         }
     });
 };
@@ -182,7 +182,7 @@ var checkSubmittedData = function () {
     request(checkSubmittedDataRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('checkSubmittedData');
             getStoreStatus();
         }
     });
@@ -192,7 +192,7 @@ var updateTip = function () {
     request(updateTipRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('updateTip');
             checkSubmittedData();
         }
     });
@@ -202,7 +202,7 @@ var deliveryInstr = function () {
     request(deliveryInstrRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('deliveryInstr');
             updateTip();
         }
     });
@@ -212,7 +212,7 @@ var getHours = function () {
     request(getHoursRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('getHours');
             getCart(deliveryInstr);
         }
     });
@@ -222,7 +222,7 @@ var checkPaymentMethods = function () {
     request(checkPaymentMethodsRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('checkPaymentMethods');
             getHours();
         }
     });
@@ -232,7 +232,7 @@ var getSchoolcash = function () {
     request(getSchoolcashRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('getSchoolcash');
             checkPaymentMethods();
         }
     });
@@ -242,7 +242,7 @@ var getTip = function () {
     request(getTipRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('getTip');
             getSchoolcash();
         }
     });
@@ -252,7 +252,7 @@ var getPaymentVars = function () {
     request(getPaymentVarsRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('getPaymentVars');
             getCart(getTip);
         }
     });
@@ -262,7 +262,7 @@ var canCheckout = function () {
     request(canCheckoutRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('canCheckout');
             getPaymentVars();
         }
     });
@@ -272,7 +272,7 @@ var addProduct = function () {
     request(addProductRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('addProduct');
             getCart(canCheckout);
         }
     });
@@ -282,7 +282,7 @@ var setDateTime = function () {
     request(dateTimeRequest, function(error, response, body) {
         if (error) throw new Error(error);
         else {
-            console.log(response);
+            console.log('setDateTime');
             getCart(addProduct);
         }
     });
@@ -292,7 +292,9 @@ var initiateOrder = function () {
     request(storeRequest, function (error, response, body) {
         if (error) throw new Error(error);
         else { 
-            console.log(response);
+            var cookie = response.caseless.dict['set-cookie'][0];
+            request = request.defaults({ headers: { 'Cookie': cookie } });
+            console.log('initiateOrder');
             setDateTime();
         }
     });
